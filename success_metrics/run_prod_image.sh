2b9c5be57871c6c8e6e845e0697c38b8b6644b9a
sotudeko <sotudeko@sonatype.com>
2020-08-13 22:30:03 +0100

    2.25

--- success_metrics/run_prod_image.sh
+++ success_metrics/run_prod_image.sh
@@ -6,6 +6,10 @@
 
 #docker run --name iq_success_metrics --rm -it -v /tmp/sm_output:/usr/src/app/output cmorenoserrano/iq_success_metrics:latest success_metrics.py -u 'http://sola.local:8070' -a admin:admin123 -s 50 -r
 
-docker run --name iq_success_metrics --rm -it -v /tmp/sm_output:/usr/src/app/output cmorenoserrano/iq_success_metrics:latest reports.py -f /usr/src/app/output/successmetrics.json -e
-docker run --name iq_success_metrics --rm -it -v /tmp/sm_output:/usr/src/app/output cmorenoserrano/iq_success_metrics:latest reports.py -f /usr/src/app/output/successmetrics.json -t
+#docker run --name iq_success_metrics --rm -it -v /tmp/sm_output:/usr/src/app/output cmorenoserrano/iq_success_metrics:latest reports.py -f /usr/src/app/output/successmetrics.json -e
+
+#docker run --name iq_success_metrics --rm -it -v /tmp/sm_output:/usr/src/app/output cmorenoserrano/iq_success_metrics:latest reports.py -f /usr/src/app/output/successmetrics.json -r
+
+docker run --name iq-success-metrics --rm -it -v /tmp/sm_output:/usr/src/app/output sonatypecommunity/iq-success-metrics:latest success_metrics.py -u http://sola.local:8070 -a admin:admin123 -s 50 -r
+
 
